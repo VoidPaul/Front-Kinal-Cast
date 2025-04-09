141c4b0376d86d58a18ed5b8fc5ebabfca49f9fe
Kinal <kinal@.edu.gt>
2025-04-09 17:16:29 -0600

    fixed: unmerged index.js file

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -4,8 +4,4 @@ export * from "./useLogout.jsx"
 export * from "./useUserDetails.jsx"
 export * from "./useChannels.jsx"
 export { useChannelDetails } from "./useChannelDetails.jsx"
-<<<<<<< HEAD
 export * from "./useFollowChannel.jsx"
-=======
-export * from './useFollowChannel.jsx'
->>>>>>> 1daa465e56363dd1f96f1d5a8427848d471868f9
